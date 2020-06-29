FIX/WIP: Clean up fixes for call stack with gradients

espei/optimizers/opt_mcmc.py
@@ -285,12 +285,9 @@ class EmceeOptimizer(OptimizerBase):
         equilibrium_thermochemical_kwargs = ctx.get('equilibrium_thermochemical_kwargs')
         starttime = time.time()
         if zpf_kwargs is not None:
-            try:
-                multi_phase_error, multi_phase_error_gradient = calculate_zpf_error(parameters=np.array(params), **zpf_kwargs)
-            except (ValueError, np.linalg.LinAlgError) as e:
-                raise e
-                print(e)
-                multi_phase_error = -np.inf
+            multi_phase_error, multi_phase_error_gradient = calculate_zpf_error(parameters=np.array(params), **zpf_kwargs)
+            multi_phase_error = multi_phase_error.sum(axis=(0, 1))
+            multi_phase_error_gradient = multi_phase_error_gradient.sum(axis=(0, 1))
         else:
             multi_phase_error = 0
         if equilibrium_thermochemical_kwargs is not None:
@@ -303,13 +300,13 @@ class EmceeOptimizer(OptimizerBase):
             # TODO
             activity_error_gradient = np.zeros(len(parameters))
         else:
-            actvity_error = 0
+            activity_error = 0
         if non_equilibrium_thermochemical_kwargs is not None:
             non_eq_thermochemical_prob = calculate_non_equilibrium_thermochemical_probability(parameters=np.array(params), **non_equilibrium_thermochemical_kwargs)
         else:
             non_eq_thermochemical_prob = 0
-        total_error = multi_phase_error + eq_thermochemical_prob + non_eq_thermochemical_prob + actvity_error
-        logging.log(TRACE, f'Likelihood - {time.time() - starttime:0.2f}s - Non-equilibrium thermochemical: {non_eq_thermochemical_prob:0.3f}. Equilibrium thermochemical: {eq_thermochemical_prob:0.3f}. ZPF: {multi_phase_error:0.3f}. Activity: {actvity_error:0.3f}. Total: {total_error:0.3f}.')
+        total_error = multi_phase_error + eq_thermochemical_prob + non_eq_thermochemical_prob + activity_error
+        logging.log(TRACE, f'Likelihood - {time.time() - starttime:0.2f}s - Non-equilibrium thermochemical: {non_eq_thermochemical_prob:0.3f}. Equilibrium thermochemical: {eq_thermochemical_prob:0.3f}. ZPF: {multi_phase_error:0.3f}. Activity: {activity_error:0.3f}. Total: {total_error:0.3f}.')
         lnlike = np.array(total_error, dtype=np.float64)
 
         lnprob = lnprior + lnlike